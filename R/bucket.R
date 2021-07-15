12d009faad444ca1f525a032e90655cc3680b6dc
Alex Buzacott <abuz5257@uni.sydney.edu.au>
2021-07-16 02:29:19 +0300

    ET in bucket model exceeding available water (#189)
    * fix bucket et
    
    * add rmarkdown to pkg suggests
    
    * Document
    
    * Remove min calls that might hide flux error
    
    * Hide min calls in bucket C code that might hide flux errrors
    
    * Document
    
    Co-authored-by: GitHub Actions <actions@github.com>
    Co-authored-by: Joseph Guillaume <josephguillaume@gmail.com>

--- R/bucket.R
+++ R/bucket.R
@@ -146,15 +146,15 @@ bucket.sim <-
         S[t] <- min(Sb, S_prev + P[t] - Eintc)
         Etrans <- M * min(1, S[t] / Sfc) * E[t]
         Ebare <- (1 - M) * (S[t] / Sb) * E[t]
-        ET[t] <- Eintc + Etrans + Ebare
+        ET[t] <- Eintc + min(S[t], Etrans + Ebare)
         ## mass balance
-        S[t] <- max(0, S_prev + P[t] - ET[t])
+        S[t] <- S_prev + P[t] - ET[t]
         ## drainage (saturation excess)
         Use <- max(0, S[t] - Sb)
-        S[t] <- max(0, S[t] - Use)
+        S[t] <- S[t] - Use
         ## drainage (sub-surface)
         Uss <- max(0, a.ss * (S[t] - Sfc))
-        S[t] <- max(0, S[t] - Uss)
+        S[t] <- S[t] - Uss
         U[t] <- Use + Uss
         S_prev <- S[t]
       }
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // hbv_sim
 DataFrame hbv_sim(NumericVector P, NumericVector E, NumericVector Tavg, double tt, double cfmax, double sfcf, double cfr, double cwh, double fc, double lp, double beta, bool initialise_sm);
 RcppExport SEXP _hydromad_hbv_sim(SEXP PSEXP, SEXP ESEXP, SEXP TavgSEXP, SEXP ttSEXP, SEXP cfmaxSEXP, SEXP sfcfSEXP, SEXP cfrSEXP, SEXP cwhSEXP, SEXP fcSEXP, SEXP lpSEXP, SEXP betaSEXP, SEXP initialise_smSEXP) {
